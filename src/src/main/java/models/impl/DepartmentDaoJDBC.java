acdc35256cb685c7afa0b98d9ed591064dbc93b4
gui-motas <monjogui@gmail.com>
2024-10-24 18:05:22 -0300

    Update DepartmentDaoJDBC.java

--- src/src/main/java/models/impl/DepartmentDaoJDBC.java
+++ src/src/main/java/models/impl/DepartmentDaoJDBC.java
@@ -6,6 +6,7 @@ import db.dbConfig;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -44,7 +45,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void deleteById(Integer id) {
-String sql = "DELETE FROM DEPARTMENT WHERE id = ?";
+        String sql = "DELETE FROM DEPARTMENT WHERE id = ?";
 
         try (Connection conexao = dbConfig.getConnection();
              PreparedStatement stmt = conexao.prepareStatement(sql)) {
@@ -57,7 +58,22 @@ String sql = "DELETE FROM DEPARTMENT WHERE id = ?";
 
     @Override
     public Department findById(Integer id) {
-        return null;
+        String sql = "SELECT * FROM DEPARTMENT WHERE id = ?";
+        try (Connection conexao = dbConfig.getConnection();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeQuery();
+            ResultSet rs = stmt.getResultSet();
+            if (rs.next()) {
+                Department department = new Department();
+                department.setId(rs.getInt("id"));
+                department.setName(rs.getString("name"));
+                return department;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       return department;
     }
 
     @Override
